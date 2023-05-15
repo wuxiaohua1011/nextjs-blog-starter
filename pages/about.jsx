763cf98e9181509dc03f0aa7bf0a3c5dd157a7ad
Michael Wu <wuxiaohua1011@gmail.com>
2023-05-15 15:48:30 -0700

    lite: build error

--- pages/about.jsx
+++ pages/about.jsx
@@ -14,7 +14,7 @@ export default function About() {
         <PageHeading>Nice to meet you.</PageHeading>
         <section className="max-w-3xl mx-auto py-8 md:py-16 lg:py-24">
           <p>
-            Join me on this exciting journey where we delve into the ever-evolving realm of technology, exploring its intricacies and unraveling its wonders. But here at Nimbus Nova, we go beyond the circuits and codes, blending in a touch of lifestyle and personal narratives to create a holistic learning experience. Whether you're a tech enthusiast, a curious learner, or simply seeking inspiration, this blog is your companion in discovering the latest trends, useful insights, and fascinating anecdotes that bridge the gap between technology and our everyday lives. So, fasten your seatbelts and prepare to embark on a thrilling adventure where knowledge meets creativity, and innovation intertwines with personal growth. 
+            Join me on this exciting journey where we delve into the ever-evolving realm of technology, exploring its intricacies and unraveling its wonders. But here at Nimbus Nova, we go beyond the circuits and codes, blending in a touch of lifestyle and personal narratives to create a holistic learning experience. Whether you&apos;re a tech enthusiast, a curious learner, or simply seeking inspiration, this blog is your companion in discovering the latest trends, useful insights, and fascinating anecdotes that bridge the gap between technology and our everyday lives. So, fasten your seatbelts and prepare to embark on a thrilling adventure where knowledge meets creativity, and innovation intertwines with personal growth. 
           </p>
           <br />
           <p>
