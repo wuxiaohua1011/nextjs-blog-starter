5ed1f4850d17f63b2f1e5bf34df3cedfc5ea47b6
Michael Wu <wuxiaohua1011@gmail.com>
2023-05-25 00:53:10 -0700

    lite: link modified

--- components/Layout/Footer.jsx
+++ components/Layout/Footer.jsx
@@ -63,10 +63,12 @@ export default function Footer() {
           >
             {navigation.main.map((item, index) => (
               <div key={index} className="px-5 py-2">
-                <Link href={item.href}>
-                  <a className="text-base text-gray-500 hover:text-gray-900 dark:hover:text-gray-200">
-                    {item.name}
-                  </a>
+                <Link
+                  href={item.href}
+                  className="text-base text-gray-500 hover:text-gray-900 dark:hover:text-gray-200">
+
+                  {item.name}
+
                 </Link>
               </div>
             ))}
@@ -91,5 +93,5 @@ export default function Footer() {
         </div>
       </Container>
     </footer>
-  )
+  );
 }
